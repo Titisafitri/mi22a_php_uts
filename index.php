b59bc550a6d51f6638b2f480a8610cdb3482a989
Titisafitri <158249048+Titisafitri@users.noreply.github.com>
2024-06-18 14:01:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* NIM : 2257401022
+* Nama : Titi Safitri
+* Kelas : MI22A
+*/
+?>
+<style>
+    #menu {
+        margin : 0;
+        padding: 0;
+    }
+    #menu a {
+        text-decoration: none;
+        color: black;
+    }
+    #menu li {
+        display: inline-block;
+        border: 1px solid gray;
+        padding: 8px;
+    }
+    #menunli:hover {
+        bacground-clor: gray;
+
+    }
+</style>
+
+<ul id="menu">
+    <li><a href="">Beranda</a></li>
+    <li><a href="2257401022_login.php">Form login</a></li>
+    <li><a href="2257401022_loop.php">Form loop</a></li>
+    <li><a href="2257401022_profil.php">Form Profil</a></li>
+</ul>
